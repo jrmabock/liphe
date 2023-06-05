20d7763257242d6ffa3096c14f02e11f5198bb53
Josh Matlack <cy.hack.lib.news.access.@gmail.com>
2023-06-05 07:51:37 -0400

    publish

---
+++ liphe/blog/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+
+# Register your models here.
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Post, Comment
+
+#add classes here for manageing post or other content from the admin panel
+
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+    list_display = ('title', 'slug', 'status','created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
